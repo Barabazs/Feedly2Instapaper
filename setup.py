983aad39d06c146e6f2dad675d2926e9391d8ed8
Dory-dev <31799121+Dory-dev@users.noreply.github.com>
2020-07-20 16:54:35 +0200

    reorganise

--- setup.py
+++ setup.py
@@ -1,12 +1,26 @@
-from setuptools import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='Feedly2Instapaper',
     version='0.1.1',
-    packages=[''],
+    packages=setuptools.find_packages(),
+    package_data={
+        'feedly2instapaper': ['settings.yaml'],
+    },
     url='https://github.com/Barabazs/Feedly2Instapaper',
     license='GNU GPLv3',
-    author='Barabasz',
+    author='Barabazs',
     author_email='',
-    description='Feedly2Instapaper adds your "Saved for later" entries to Instapaper and removes those entries in Feedly.'
+    description='Feedly2Instapaper adds your "Saved for later" entries to Instapaper and removes those entries in Feedly.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU GPLv3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
