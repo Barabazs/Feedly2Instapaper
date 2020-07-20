54584260c185001aa8d35557ae8a10dcdf3e7b99
Dory-dev <31799121+Dory-dev@users.noreply.github.com>
2020-07-20 14:10:36 +0200

    Add requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Feedly2Instapaper',
+    version='0.1.1',
+    packages=[''],
+    url='https://github.com/Barabazs/Feedly2Instapaper',
+    license='GNU GPLv3',
+    author='Barabasz',
+    author_email='',
+    description='Feedly2Instapaper adds your "Saved for later" entries to Instapaper and removes those entries in Feedly.'
+)
