f00128f2124addd9f3630797ac09e9addbd15a9b
Barabazs <31799121+Barabazs@users.noreply.github.com>
2022-11-06 10:21:16 +0100

    refacor: setup

--- setup.py
+++ setup.py
@@ -1,20 +1,24 @@
-import setuptools
+from setuptools import find_packages, setup
+
+extras_require = {
+    "dev": ["pre-commit", "black", "isort"],
+    "test": [],
+}
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name='Feedly2Instapaper',
-    version='0.1.2',
-    packages=setuptools.find_packages(),
+setup(
+    name="Feedly2Instapaper",
+    version="0.1.4",
     package_data={
-        'feedly2instapaper': ['settings.yaml'],
+        "feedly2instapaper": ["settings.yaml"],
     },
-    install_requires=['setuptools', 'instapaper', 'feedly-client', 'PyYAML'],
-    url='https://github.com/Barabazs/Feedly2Instapaper',
-    license='GNU GPLv3',
-    author='Barabazs',
-    author_email='',
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    url="https://github.com/Barabazs/Feedly2Instapaper",
+    license="MIT",
+    author="Barabazs",
+    author_email="",
     description='Feedly2Instapaper adds your "Saved for later" entries to Instapaper and removes those entries in Feedly.',
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -23,5 +27,11 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
+    install_requires=[
+        "instapaper==0.4",
+        "PyYAML~=6.0",
+        "feedly-client==0.25",
+    ],
+    extras_require=extras_require,
 )
